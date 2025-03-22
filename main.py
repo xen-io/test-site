fd72be7bd45ea756f37e0e9cbe85d2b555290495
myname <me@me.com>
2025-03-22 13:44:41 +0500

    feat: full name in owners and formatted sex column

--- main.py
+++ main.py
@@ -18,8 +18,34 @@ with open('owners.html') as f:
 with open('animals.html') as f:
     animals_html = f.read()
 
-
 app = FastAPI()
+
+
+async def owners_frame():
+    query = '''SELECT * FROM owners'''
+    # ответ на запрос query засовывается в frame типа DataFrame
+    frame = pd.DataFrame(await data['connection'].fetch(query))
+    frame.columns = ['ID', 'Имя', 'Фамилия', 'Отчество', 'Пол', 'Дата рождения', 'Номер дома']
+    # форматируется колонка Пол
+    frame['Пол'] = frame['Пол'].apply(lambda x: 'Ж' if x else 'М')
+    return frame
+
+
+async def animals_frame():
+    query = '''
+    SELECT
+        animal_id, CONCAT(owner_lastname, ' ', owner_name, ' ', owner_middlename),
+        animal_nick, animal_type,
+        animal_sex, animal_birthday,
+        animal_reg_date, animal_description
+    FROM
+        animals LEFT JOIN owners ON animals.owner_id = owners.owner_id'''
+    # ответ на запрос query засовывается в frame типа DataFrame
+    frame = pd.DataFrame(await data['connection'].fetch(query))
+    frame.columns = ['ID', 'ФИО Владельца', 'Кличка', 'Тип', 'Пол', 'Дата рождения', 'Дата регистрации', 'Описание']
+    # форматируется колонка Пол
+    frame['Пол'] = frame['Пол'].apply(lambda x: 'Ж' if x else 'М')
+    return frame
 
 
 @app.on_event('startup')
@@ -34,13 +60,9 @@ async def index():
 
 @app.get('/owners', response_class=HTMLResponse)
 async def owners():
-    frame: pd.DataFrame = pd.DataFrame(await data['connection'].fetch('SELECT * FROM owners'))
-    frame.columns = ['ID', 'Имя', 'Фамилия', 'Отчество', 'Пол', 'Дата рождения', 'Номер дома']
-    return owners_html.replace('TABLE_DATA', frame.to_html(index=False, justify='center'))
+    return owners_html.replace('TABLE_DATA', (await owners_frame()).to_html(index=False, justify='center'))
 
 
 @app.get('/animals', response_class=HTMLResponse)
 async def animals():
-    frame: pd.DataFrame = pd.DataFrame(await data['connection'].fetch('SELECT * FROM animals'))
-    frame.columns = ['ID', 'ID Владельца', 'Кличка', 'Тип', 'Пол', 'Дата рождения', 'Дата регистрации', 'Описание']
-    return animals_html.replace('TABLE_DATA', frame.to_html(index=False, justify='center'))
+    return animals_html.replace('TABLE_DATA', (await animals_frame()).to_html(index=False, justify='center'))
